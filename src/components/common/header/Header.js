a3d8b36bf12b35d1b62b2eebffa02be69c398bb9
ArantxaDR <arantxaderuiz@gmail.com>
2023-02-19 19:49:20 +0100

    fix styles

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from "react-router-dom";
 import './Header.scss';
 import shoping from '../../../assets/images/shopping-cart.svg';
+import nautilus from '../../../assets/images/nautilus.png'; 
 
 
 
@@ -9,11 +10,11 @@ export const Header = ({products}) => {
 
 	return (
 		<header className='header_container'>
-			<Link>
-				<h1>Napptilus</h1>
+			<Link to ="/">
+				<img className='header_container__logo' src={nautilus} alt='Eccomerce logo'/>
 			</Link>
 			<div>
-				<img className='header_container-icon' src={shoping} alt='Shopping cart'/>
+				<img className='header_container__icon' src={shoping} alt='Shopping cart'/>
 			</div>
 	  </header>
   )
